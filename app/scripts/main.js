34ddf3b005f0a8f232f64c26a77761c025dea51a
user.email <will.hakes@vantiv.com>
2017-08-22 10:20:09 -0600

    Fix TypeScript Type errors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,16 @@
 function myFunction() {
-    var x = document.getElementById("myFile").files[0];
+    var myfile = document.getElementById("myFile");
+    var x = myfile.files[0];
     var fr = new FileReader();
     fr.onload = function (e) {
-        document.getElementById("output").innerHTML = fr.result;
+        var text1 = document.getElementById("text1");
+        text1.value = fr.result;
+        parseXML(fr.result);
     };
     fr.readAsText(x);
+}
+function parseXML(xmlinput) {
+    var parser = new DOMParser();
+    var the_dom = parser.parseFromString(xmlinput.toString(), "application/xml");
 }
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 function myFunction() {
-    var x = document.getElementById("myFile").files[0];
+    var myfile = <HTMLInputElement> document.getElementById("myFile");
+    var x = myfile.files[0];
     var fr = new FileReader();
     fr.onload = function(e){
-      document.getElementById("output").innerHTML = fr.result;
+      var text1 = <HTMLInputElement> document.getElementById("text1");
+      text1.value = fr.result;
+      parseXML(fr.result);
     };
     fr.readAsText(x);
+
+}
+
+function parseXML(xmlinput: String) {
+  var parser = new DOMParser();
+  var the_dom = parser.parseFromString(xmlinput.toString(), "application/xml");
 
 }
