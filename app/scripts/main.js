b464238e094e7012cf942708890cd1e0c8837b1e
user.email <will.hakes@vantiv.com>
2017-08-22 13:16:45 -0600

    Breakout getters from parseXML

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,11 +13,25 @@ function parseXML(xmlinput) {
     var parser = new DOMParser();
     var the_dom = parser.parseFromString(xmlinput.toString(), "application/xml");
     var tripos = the_dom.getElementsByTagName('tripos')[0];
+    configVersion_field.value = get_value(tripos, "configVersion");
+    developerKey_field.value = get_value(tripos, "developers/developer/developerKey");
+}
+function get_configVersion(tripos) {
     var configVersion = tripos.getElementsByTagName('configVersion')[0];
-    configVersion_field.value = configVersion.textContent;
+    return configVersion.textContent;
+}
+function get_developerKey(tripos) {
     var developers = tripos.getElementsByTagName('developers')[0];
     var developer = developers.getElementsByTagName('developer')[0];
     var developerKey = developer.getElementsByTagName('developerKey')[0];
-    developerKey_field.value = developerKey.textContent;
+    return developerKey.textContent;
+}
+function get_value(tripos, path) {
+    var location = tripos;
+    var checkpoints = path.split("/");
+    checkpoints.forEach(function (cp) {
+        location = location.getElementsByTagName(cp)[0];
+    });
+    return location.textContent;
 }
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -19,11 +19,32 @@ function parseXML(xmlinput: String) {
   var the_dom = parser.parseFromString(xmlinput.toString(), "application/xml");
   var tripos = the_dom.getElementsByTagName('tripos')[0];
 
-  var configVersion = tripos.getElementsByTagName('configVersion')[0];
-  configVersion_field.value = configVersion.textContent;
+  //configVersion_field.value = <string> get_configVersion(tripos);
+  //developerKey_field.value = <string> get_developerKey(tripos);
 
+  configVersion_field.value = <string> get_value(tripos, "configVersion");
+  developerKey_field.value = <string> get_value(tripos, "developers/developer/developerKey");
+}
+
+function get_configVersion(tripos: Element): String {
+  var configVersion = tripos.getElementsByTagName('configVersion')[0];
+  return configVersion.textContent;
+}
+
+function get_developerKey(tripos: Element): String {
   var developers = tripos.getElementsByTagName('developers')[0];
   var developer = developers.getElementsByTagName('developer')[0];
   var developerKey = developer.getElementsByTagName('developerKey')[0];
-  developerKey_field.value = developerKey.textContent;
+  return developerKey.textContent;
+}
+
+function get_value(tripos:Element, path: String): String {
+  var location = tripos;
+  var checkpoints = path.split("/");
+
+  checkpoints.forEach( function(cp) {
+    location = location.getElementsByTagName(cp)[0];
+  })
+
+  return location.textContent;
 }
