22d56f348847c89474a99fa004954432fa79626b
user.email <will.hakes@vantiv.com>
2017-08-22 11:51:46 -0600

    Add deep parsing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,18 +3,21 @@ function myFunction() {
     var x = myfile.files[0];
     var fr = new FileReader();
     fr.onload = function (e) {
-        var text1 = document.getElementById("text1");
-        text1.value = fr.result;
         parseXML(fr.result);
     };
     fr.readAsText(x);
 }
 function parseXML(xmlinput) {
+    var text1 = document.getElementById("text1");
+    var text2 = document.getElementById("text2");
     var parser = new DOMParser();
     var the_dom = parser.parseFromString(xmlinput.toString(), "application/xml");
     var tripos = the_dom.getElementsByTagName('tripos')[0];
     var configVersion = tripos.getElementsByTagName('configVersion')[0];
-    var text2 = document.getElementById("text2");
-    text2.value = configVersion.textContent.toString();
+    text1.value = configVersion.textContent;
+    var developers = tripos.getElementsByTagName('developers')[0];
+    var developer = developers.getElementsByTagName('developer')[0];
+    var developerKey = developer.getElementsByTagName('developerKey')[0];
+    text2.value = developerKey.textContent;
 }
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ function myFunction() {
     var x = myfile.files[0];
     var fr = new FileReader();
     fr.onload = function(e){
-      var text1 = <HTMLInputElement> document.getElementById("text1");
-      text1.value = fr.result;
+      //var text1 = <HTMLInputElement> document.getElementById("text1");
+      //text1.value = fr.result;
       parseXML(fr.result);
     };
     fr.readAsText(x);
@@ -12,10 +12,18 @@ function myFunction() {
 }
 
 function parseXML(xmlinput: String) {
-    var parser = new DOMParser();
-    var the_dom = parser.parseFromString(xmlinput.toString(), "application/xml");
-    var tripos = the_dom.getElementsByTagName('tripos')[0];
-    var configVersion = tripos.getElementsByTagName('configVersion')[0];
-    var text2 = <HTMLInputElement> document.getElementById("text2");
-    text2.value = configVersion.textContent.toString();
+  var text1 = <HTMLInputElement> document.getElementById("text1");
+  var text2 = <HTMLInputElement> document.getElementById("text2");
+
+  var parser = new DOMParser();
+  var the_dom = parser.parseFromString(xmlinput.toString(), "application/xml");
+  var tripos = the_dom.getElementsByTagName('tripos')[0];
+
+  var configVersion = tripos.getElementsByTagName('configVersion')[0];
+  text1.value = configVersion.textContent;
+
+  var developers = tripos.getElementsByTagName('developers')[0];
+  var developer = developers.getElementsByTagName('developer')[0];
+  var developerKey = developer.getElementsByTagName('developerKey')[0];
+  text2.value = developerKey.textContent;
 }
