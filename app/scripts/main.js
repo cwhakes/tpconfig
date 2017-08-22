2dda56c12fa0a98bd94ff46000d30c48df3dca40
user.email <will.hakes@vantiv.com>
2017-08-22 16:31:07 -0600

    compile validation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,24 +13,12 @@ function parseXML(xmlinput) {
     var parser = new DOMParser();
     var the_dom = parser.parseFromString(xmlinput.toString(), "application/xml");
     var tripos = the_dom.getElementsByTagName('tripos')[0];
-    paths().split(" ").filter(checkEmpty).forEach(function (path) {
+    paths().split(" ").forEach(function (path) {
         var checkpoints = path.split("/");
         var element_name = checkpoints[checkpoints.length - 1];
         var field = document.getElementById(element_name);
-        try {
-            field.value = get_value(tripos, path);
-        }
-        catch (err) {
-            var ignore = err;
-            field.value = "ERROR";
-            field.style.backgroundColor = "Red";
-        }
+        field.value = get_value(tripos, path);
     });
-}
-function checkEmpty(str) {
-    if (str.trim().length > 0) {
-        return str;
-    }
 }
 function get_value(tripos, path) {
     var location = tripos;
@@ -46,6 +34,38 @@ function paths() {
     developers/developer/developerKey \
     developers/developer/developerSecret \
     server/listeningPort \
+	host/acceptorId \
+	host/accountId \
+	host/accountToken \
+	host/autoReversalRetryLimit \
+	application/pinPadIdleMessage \
+	application/corsAllowedOrigins \
+	transaction/CreditAvsEntryCondition \
+	transaction/cashbackSelections \
+	transaction/cashbackAmountIncrement \
+	transaction/maximumCashbackAmount \
+	transaction/tipSelections \
+	transaction/debitSurcharge \
+	transaction/creditSaleSignatureThresholdAmount \
+	transaction/signatureFormat \
+	transaction/emvFallbackAllowed \
+	lanes/serialLane/description \
+	lanes/serialLane/laneID \
+	lanes/serialLane/pinpad/comPort \
+	lanes/serialLane/pinpad/dataBits \
+	lanes/serialLane/pinpad/parity \
+	lanes/serialLane/pinpad/stopBits \
+	lanes/serialLane/host/terminalId \
   ";
+}
+function validate_developerSecret(self) {
+    var data = self.value.split("-").join("");
+    var array = data.match("^[0-9a-fA-F]*$");
+    if (array != null) {
+        self.style.backgroundColor = "white";
+    }
+    else {
+        self.style.backgroundColor = "red";
+    }
 }
 //# sourceMappingURL=main.js.map
