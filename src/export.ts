00b1231251e724d21ebc71533c41c4e49dd8c569
user.email <will.hakes@vantiv.com>
2017-08-23 13:39:06 -0600

    add download button

--- src/export.ts
+++ src/export.ts
@@ -17,3 +17,27 @@ function exportXml(dom: Document):void {
 	file.Close();
 }
 */
+
+
+function exportXml(): void {
+	var data: String = window.sessionStorage.the_dom;
+	download(data, "triposconfig", "XML");
+}
+
+function download(data:any, filename:any, type:any) {
+	var file = new Blob([data], {type: type});
+	if (window.navigator.msSaveOrOpenBlob) {//IE10
+		window.navigator.msSaveOrOpenBlob(file, filename);
+	} else {
+		var a = document.createElement("a"),
+			url = URL.createObjectURL(file);
+		a.href = url;
+		a.download = filename;
+		document.body.appendChild(a);
+		a.click();
+		setTimeout(function() {
+			document.body.removeChild(a);
+			window.URL.revokeObjectURL(url);
+		}, 0);
+	}
+}
