f1fa646655012cb7ed351592cb047433fd349353
Anthei <prowler86@gmail.com>
2017-08-23 08:17:09 -0600

    add comment line 2 to export.ts

--- src/export.ts
+++ src/export.ts
@@ -10,6 +10,7 @@ function exportXml():void {
 	file.WriteLine('<?xml version="1.0" encoding="utf-8"?>\n');
 	file.WriteLine('<tripos xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\n');
 	file.WriteLine('<!-- This is a comment-->\n');
+	file.WriteLine('<!-- This is comment 2-->\n');
 	file.WriteLine('</tripos>');
 
 	file.Close();
