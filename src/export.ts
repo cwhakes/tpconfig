a5f6338812aa846f8181528f283713b48f93e565
Anthei <prowler86@gmail.com>
2017-08-23 09:03:19 -0600

    add a test write to xml doc in export.ts

--- src/export.ts
+++ src/export.ts
@@ -4,12 +4,12 @@ var fso = new ActiveXObject("Scripting.FileSystemObject");
 var FILENAME = 'test.xml';
 
 //dom:Document
-function exportXml():void {
+function exportXml(dom: Document):void {
 
 	var file = fso.CreateTextFile(FILENAME, true);
 	file.WriteLine('<?xml version="1.0" encoding="utf-8"?>\n');
 	file.WriteLine('<tripos xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\n');
-	file.WriteLine('<!-- This is a comment-->\n');
+	file.WriteLine('<' + dom.getElementById("configVersion").id + '>' dom.getElementById("configVersion").value + '</' + dom.getElementById("configVersion").id + '>\n');
 	file.WriteLine('<!-- This is comment 2-->\n');
 	file.WriteLine('</tripos>');
 
