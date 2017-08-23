5efc3210b6b9cde48f78a8a442be97a1431aced6
user.email <will.hakes@vantiv.com>
2017-08-22 22:06:49 -0600

    make validateHex check for empty

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -120,7 +120,7 @@ function validateInput(event) {
 function validateHex(self) {
     var data = self.value.split("-").join("");
     var array = data.match("^[0-9a-fA-F]*$");
-    if (array != null) {
+    if (array != null && array.length > 0) {
         self.style.backgroundColor = "white";
     }
     else {
--- src/validate.ts
+++ src/validate.ts
@@ -6,7 +6,7 @@ function validateInput(event: Event) {
 function validateHex(self: HTMLInputElement): void {
   var data = <String> self.value.split("-").join("");
   var array = data.match("^[0-9a-fA-F]*$");
-  if (array != null) {
+  if (array != null && array.length > 0) {
     self.style.backgroundColor = "white";
   } else {
     self.style.backgroundColor = "red";
