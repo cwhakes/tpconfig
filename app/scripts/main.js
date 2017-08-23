a5a59c2158775643b60957372d69042006cb2453
user.email <will.hakes@vantiv.com>
2017-08-23 09:10:31 -0600

    fix type error

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -136,11 +136,12 @@ function validateBool(self) {
 var xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
 var fso = new ActiveXObject("Scripting.FileSystemObject");
 var FILENAME = 'test.xml';
-function exportXml() {
+function exportXml(dom) {
     var file = fso.CreateTextFile(FILENAME, true);
     file.WriteLine('<?xml version="1.0" encoding="utf-8"?>\n');
     file.WriteLine('<tripos xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\n');
-    file.WriteLine('<!-- This is a comment-->\n');
+    var configVersion = dom.getElementById("configVersion");
+    file.WriteLine('<' + dom.getElementById("configVersion").id + '>' + configVersion.value + '</' + dom.getElementById("configVersion").id + '>\n');
     file.WriteLine('<!-- This is comment 2-->\n');
     file.WriteLine('</tripos>');
     file.Close();
--- src/export.ts
+++ src/export.ts
@@ -9,7 +9,8 @@ function exportXml(dom: Document):void {
 	var file = fso.CreateTextFile(FILENAME, true);
 	file.WriteLine('<?xml version="1.0" encoding="utf-8"?>\n');
 	file.WriteLine('<tripos xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\n');
-	file.WriteLine('<' + dom.getElementById("configVersion").id + '>' + dom.getElementById("configVersion").value + '</' + dom.getElementById("configVersion").id + '>\n');
+	var configVersion = <HTMLInputElement> dom.getElementById("configVersion");
+	file.WriteLine('<' + dom.getElementById("configVersion").id + '>' + configVersion.value + '</' + dom.getElementById("configVersion").id + '>\n');
 	file.WriteLine('<!-- This is comment 2-->\n');
 	file.WriteLine('</tripos>');
 
